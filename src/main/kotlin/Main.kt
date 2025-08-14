988206b8d17d17214894f867e48f6e78be246fb9
QHailRakeQ <volodymyr.pohorelenko@gmail.com>
2025-08-14 18:08:32 +0200

    Bubble sort unreadable output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
-package org.example
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun bubbleSort(arr: IntArray) {
+    for(i in 0 until arr.size - 1) {
+        for(j in 0 until arr.size - i - 1) {
+            if(arr[j] > arr[j + 1]) {
+                val temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
     }
+}
+fun main() {
+   val nums = intArrayOf(1, 5, 7, 4, 9, 10)
+    bubbleSort(nums)
+    println("Sorted array: $nums")
 }
